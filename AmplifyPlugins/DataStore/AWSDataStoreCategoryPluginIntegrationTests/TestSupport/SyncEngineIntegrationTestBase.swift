bac8a71f762df0b5a289fc34b5e0140a2be9f288
Daniel Rochetti <rochetti@amazon.com>
2020-03-31 17:48:25 -0700

    add DataStore configuration API

--- AmplifyPlugins/DataStore/AWSDataStoreCategoryPluginIntegrationTests/TestSupport/SyncEngineIntegrationTestBase.swift
+++ AmplifyPlugins/DataStore/AWSDataStoreCategoryPluginIntegrationTests/TestSupport/SyncEngineIntegrationTestBase.swift
@@ -41,8 +41,8 @@ class SyncEngineIntegrationTestBase: XCTestCase {
         Amplify.Logging.logLevel = .verbose
 
         do {
-            try Amplify.add(plugin: AWSAPIPlugin(modelRegistration: TestModelRegistration()))
-            try Amplify.add(plugin: AWSDataStorePlugin(modelRegistration: TestModelRegistration()))
+            try Amplify.add(plugin: AWSAPIPlugin())
+            try Amplify.add(plugin: AWSDataStorePlugin(schema: TestDataStoreSchema()))
         } catch {
             XCTFail(String(describing: error))
             return
